4ab62a02c6a69e3118c58d99e65959b329f399cc
raaukr <raaukr@gmail.com>
2023-02-03 23:22:50 +0200

    #4 spinner

--- main.py
+++ main.py
@@ -1,6 +1,48 @@
+import random
+
 MAX_LINES = 3
 MIN_BET = 1
 MAX_BET = 100
+
+ROWS = 3
+COLS = 3
+
+symbol_count = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8
+}
+
+def get_slot_machine_spin(rows, cols, symbols):
+    all_symbols = []
+    for symbol, symbol_count in symbols.items():
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+
+    columns = []
+    for col in range(cols):
+        column = []
+        current_symbols = all_symbols[:]
+        for row in range(rows):
+            value = random.choice(current_symbols)
+            current_symbols.remove(value)
+            column.append(value)
+
+        columns.append(column)
+
+    return columns
+
+def print_slot_machine(columns):
+    for row in range(len(columns[0])):
+        for i, column in enumerate(columns):
+            if i != len(columns) - 1:
+                print(column[row], end=" | ")
+            else:
+                print(column[row], end="")
+        print()
+
+
 
 def deposit():
     while True:
@@ -13,7 +55,7 @@ def deposit():
                 print('Type number greater 0.\n')
         else:
             print('Please enter the number.\n')
-    
+
     return(amount)
 
 def lines():
@@ -27,7 +69,7 @@ def lines():
                 print('Type number 1 - ' + str(MAX_LINES) + '.\n')
         else:
             print('Please enter the number.\n')
-    
+
     return(lines)
 
 def bet():
@@ -41,14 +83,23 @@ def bet():
                 print(f'Type the number between {MIN_BET} - {MAX_BET}.\n')
         else:
             print('Please enter the number.\n')
-    
-    return(amount)   
+
+    return(amount)
 
 def main():
     balance = deposit()
-    lines = lines()
-    bet = bet()
-    total_bet = bet * lines
+    l = lines()
+    b = bet()
+    total_bet = b * l
+    while True:
+        if total_bet <= balance:
+            break
+        else:
+            print(f'Your bet is bigger than bet. ({total_bet} > {balance})')
+
     print(f'Your bet is {total_bet}.')
+
+    slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
+    print_slot_machine(slots)
 
 main()
